65cf1a7e605f2851847ca1423baa846dbc265aa7
IGernhardt <isaiah@igernhardt.com>
2024-02-01 03:09:57 -0500

    Program.cs: Allowed JSON Enum Conversion

--- SpaceXApiWrapper/Program.cs
+++ SpaceXApiWrapper/Program.cs
@@ -3,13 +3,17 @@ using Microsoft.OpenApi.Models;
 using SpaceXApiWrapper.Repositories;
 using Swashbuckle.AspNetCore.Filters;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IIncidentRepository, IncidentRepository>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+    );
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
